be70bcf58eb43eaed960217bfb9aab9ac11ae62a
Progress Ochuko Eyaadah <koxygraphics@gmail.com>
2023-12-09 23:42:38 +0100

    made edits

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,44 +3,30 @@ const { ethers } = require('hardhat');
 
 async function main() {
   const [deployer] = await ethers.getSigners();
+  
 
 
   if (network.name === "sepolia") {
-    // Deploy xCallETH contract
+   
     const xCallETH = await ethers.getContractFactory("xCallETH");
     const xCallEthContract = await xCallETH.deploy();
 
     await xCallEthContract.deployed();
     console.log("xCallETH deployed to:", xCallEthContract.address);
 
-    // // Initialize xCallETH contract for sepolia network
-    // const xCallETHInitTx = await xCallEthContract.initialize(
-    //   xCallBSCContract.address,
-    //   "YOUR_DESTINATION_ADDRESS_SEPOLIA"
-    // );
-
-    // await xCallETHInitTx.wait();
-    // console.log("xCallETH initialized for sepolia network");
   }
 
   if (network.name === "bscTestnet") {
 
-    // Deploy xCallETH contract
     const xCallBSC = await ethers.getContractFactory("xCallBSC");
     const xCallBSCContract = await xCallBSC.deploy();
 
 
     await xCallBSCContract.deployed();
+    const BSC_ADDRESS = xCallBSCContract.address;
+    console.log("BSC_ADDRESS", BSC_ADDRESS);
     console.log("xCallBSC deployed to:", xCallBSCContract.address);
 
-    // // Initialize xCallBSC contract for bscTestnet network
-    // const xCallBSCInitTx = await xCallBSCContract.initialize(
-    //   xCallEthContract.address,
-    //   "YOUR_DESTINATION_ADDRESS_BSC_TESTNET"
-    // );
-
-    // await xCallBSCInitTx.wait();
-    // console.log("xCallBSC initialized for bscTestnet network");
   }
 }
 
--- scripts/initialize.js
+++ scripts/initialize.js
@@ -1,40 +1,34 @@
-// initialize.js
-const BridgeEth = require('../artifacts/contracts/BridgeEth.sol/BridgeEth.json');
-const BridgeBsc = require('../artifacts/contracts/BridgeEth.sol/BridgeEth.json');
-
-const { ethers } = require("hardhat");
-const { abi } = require("../artifacts/contracts/xCallETH..sol/xCallETH.json");
-
 async function main() {
   const [deployer] = await ethers.getSigners();
 
   console.log("Initializing contracts with the account:", deployer.address);
 
   // Replace these with the actual contract addresses on the target network
-  const xCallEthAddress = "0x533378eCb7384d70d97e6A68C02137F894c3aa47";
+  const xCallETHAddress = "0x533378eCb7384d70d97e6A68C02137F894c3aa47";
   const xCallBSCAddress = "0xa140c305EA4F9F9b962354FE54e6D4C0417912D5";
 
   // Initialize xCallETH contract
   if (network.name === "sepolia") {
-    const xCallETH = await ethers.getContractAt("xCallETH");
+    const xCallETH = await ethers.getContractAt("xCallETH", xCallETHAddress);
     const xCallETHInitTx = await xCallETH.initialize(
       xCallBSCAddress,
       "0xC938B1B7C20D080Ca3B67eebBfb66a75Fb3C4995"
     );
-
     await xCallETHInitTx.wait();
     console.log("xCallETH initialized");
   }
 
   // Initialize xCallBSC contract
-  const xCallBSC = await ethers.getContractAt("xCallBSC", xCallBSCAddress);
-  const xCallBSCInitTx = await xCallBSC.initialize(
-    xCallETHAddress,
-    "0x8E302b2fD7C10A0033e48EB0602Db3C7d6E0F506"
-  );
+  if (network.name === "bscTestnet") {
+    const xCallBSC = await ethers.getContractAt("xCallBSC", xCallBSCAddress);
+    const xCallBSCInitTx = await xCallBSC.initialize(
+      xCallETHAddress,
+      "0x8E302b2fD7C10A0033e48EB0602Db3C7d6E0F506"
+    );
 
-  await xCallBSCInitTx.wait();
-  console.log("xCallBSC initialized");
+    await xCallBSCInitTx.wait();
+    console.log("xCallBSC initialized");
+  }
 }
 
 main()
